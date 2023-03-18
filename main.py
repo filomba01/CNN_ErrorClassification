e6fac69480f7105c94eab1104301d5da3423a209
filippo <filo.balza2001@gmail.com>
2023-03-18 12:04:48 +0100

    improved main.py with directory selection, created plotTensor.py for plotting elements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 from sys import argv, exit
 from math import sqrt
 import os
@@ -12,48 +12,24 @@ DEPTH = 2
 CHANNEL = 3
 # end def
 
-PLOT = True
-save = True
-
-
-# title = ''
-
-
-def split_two(num):
-    if is_square(num):
-        return int(sqrt(num)), int(sqrt(num))
-
-    radix = int(sqrt(num))
-
-    while num % radix != 0:
-        radix -= 1
-
-    return radix, num // radix
-
-
-def is_square(apositiveint):
-    x = apositiveint // 2
-    seen = set([x])
-    while x * x != apositiveint:
-        x = (x + (apositiveint // x)) // 2
-        if x in seen: return False
-        seen.add(x)
-    return True
-
-
-golden = np.load(argv[1])[0, ...]
 if len(os.path.abspath(__file__).split('/')) > 1:
     separator = '/'
 else:
     separator = '\\'
 filename = separator + os.path.abspath(__file__).split(separator)[-1]
 os.path.abspath(__file__).replace(filename, '')
-path = os.path.abspath(__file__).replace(filename, '') + separator + 'tensors_corrupted'
-os.chdir(path)
+path = os.path.abspath(__file__).replace(filename, '') + separator + 'tensors_corrupted/experiments'
+choosenTestFolder = input("Insert the folder: ")
+choosenTensorsF = input("Insert the subfolder: ")
+path = path + separator + choosenTestFolder
+golden = np.load(path+'/output_1.npy')
+#print(golden)
+os.chdir(path + separator + choosenTensorsF + separator)
 
 for file in os.listdir():
     if file.endswith(".npy"):
-        file_path = path + separator + file
+        file_path = path + separator + choosenTensorsF + separator + file
+        #print(file_path)
         faulty = np.load(file_path)
 
         # variables for classification
@@ -63,70 +39,55 @@ for file in os.listdir():
         shatteredGlass = True
 
         # diff cube generation
-        diff_cube = np.where(golden - faulty)
+        diffs = np.abs(golden - faulty)
+        diff_cube = np.where(diffs > 1e-3)
         temp = [(diff_cube[j][i]) for i in range(len(diff_cube[0])) for j in range(len(diff_cube))]
         diff_cube = [tuple(temp[n:n + len(diff_cube)]) for n in range(0, len(temp), len(diff_cube))]
 
         diff_cube = sorted(diff_cube, key=lambda x: x[3])
+        print(diff_cube)
         # errors
         if (size(diff_cube) / 4) > 1:
             singlePoint = False
-        rReference = diff_cube[0][ROW]
-        cReference = diff_cube[0][COLUMN]
-        dReference = diff_cube[0][DEPTH]
-        chReference = diff_cube[0][CHANNEL]
-        lastChSeen = chReference
-        Range = int(size(diff_cube) / 4)
-        atLeastBullet = False
-        for k in range(1, Range):
-            # check if exists an element that ensures that the error can be atLeastBullet
-            if diff_cube[k][ROW] == rReference and diff_cube[k][COLUMN] == cReference and diff_cube[k][DEPTH] == dReference and not atLeastBullet:
-                atLeastBullet = True
+            rReference = diff_cube[0][ROW]
+            cReference = diff_cube[0][COLUMN]
+            dReference = diff_cube[0][DEPTH]
+            chReference = diff_cube[0][CHANNEL]
+            lastChSeen = chReference
+            Range = int(size(diff_cube) / 4)
+            print(Range)
+            atLeastBullet = False
+            for k in range(1, Range):
+                # check if exists an element that ensures that the error can be atLeastBullet
+                if diff_cube[k][ROW] == rReference and diff_cube[k][COLUMN] == cReference and diff_cube[k][DEPTH] == dReference and not atLeastBullet:
+                    atLeastBullet = True
 
-            #  common conditions
-            if diff_cube[k][ROW] != rReference or diff_cube[k][COLUMN] != cReference:
-                isSameRow = False
-                shatteredGlass = False
-                bulletWake = False
-
-            # same row conditions
-            if diff_cube[k][CHANNEL] != chReference:
-                isSameRow = False
-
-            # bulletWake condition
-            if diff_cube[k][ROW] == rReference and diff_cube[k][COLUMN] == cReference and diff_cube[k][DEPTH] != dReference:
-                bulletWake = False
-
-            # triggers after the complete analysis of one channel
-            if lastChSeen != diff_cube[k][CHANNEL]:
-                lastChSeen = diff_cube[k][CHANNEL]
-                if not atLeastBullet:
-                    bulletWake = False
+                #  common conditions
+                if diff_cube[k][ROW] != rReference or diff_cube[k][COLUMN] != cReference:
+                    isSameRow = False
                     shatteredGlass = False
-                else:
-                    atLeastBullet = False
-        # adjusting the right classification
-        if bulletWake:
-            shatteredGlass = False
+                    bulletWake = False
 
+                # same row conditions
+                if diff_cube[k][CHANNEL] != chReference:
+                    isSameRow = False
 
-        if PLOT:
-            # print
-            faulty = np.load(file_path)[0, ...]
-            channels = golden.shape[2]
-            x, y = split_two(channels)
-            fig, axs = plt.subplots(x, y)
-            for j in range(y):
-                for i in range(x):
-                    diff = np.abs(golden[:, :, i + j * x] - faulty[:, :, i + j * x])
-                    diff = np.where(diff < 1e-3, 0, 1)
-                    axs[i, j].imshow(diff, cmap='hot', interpolation='nearest')
-                    axs[i, j].set_yticks([])
-                    axs[i, j].set_xticks([])
-                    axs[i, j].set_yticklabels([])
-                    axs[i, j].set_xticklabels([])
-                    axs[i, j].set_title(f'Channel {i + j * x}', fontsize=8)
-            # end print
+                # bulletWake condition
+                if diff_cube[k][ROW] == rReference and diff_cube[k][COLUMN] == cReference and diff_cube[k][DEPTH] != dReference:
+                    bulletWake = False
+
+                # triggers after the complete analysis of one channel
+                if lastChSeen != diff_cube[k][CHANNEL]:
+                    lastChSeen = diff_cube[k][CHANNEL]
+                    if not atLeastBullet:
+                        bulletWake = False
+                        shatteredGlass = False
+                    else:
+                        atLeastBullet = False
+            # adjusting the right classification
+            if bulletWake:
+                shatteredGlass = False
+
 
         # from the line command 1)Linux 2)Windows
         # 1) tensor_name = argv[2].split('/')[-1].split('.')[0]
@@ -139,19 +100,28 @@ for file in os.listdir():
             tensor_name = file_path.split('.')[0].split("\\")[-1]
         path2err = os.path.abspath(__file__).replace(filename, '') + separator + 'error_classes' + separator
         if len(diff_cube) == 1:
-            title = path2err + 'single_point' + separator + tensor_name
+            errorType = 'single_point'
+            title = path2err + 'single_point' + separator
         elif isSameRow:
-            title = path2err + 'same_row' + separator + tensor_name
+            errorType = 'same_row'
+            title = path2err + 'same_row' + separator
         elif bulletWake and (len(diff_cube) > 1):
-            title = path2err + 'bullet_wake' + separator + tensor_name
+            errorType = 'bullet_wake'
+            title = path2err + 'bullet_wake' + separator
         elif shatteredGlass:
-            title = path2err + 'shattered_glass' + separator + tensor_name
+            errorType = 'shattered_glass'
+            title = path2err + 'shattered_glass' + separator
         else:
-            title = path2err + 'undefined_error' + separator + tensor_name
+            errorType = 'undefined_error'
+            title = path2err + 'undefined_error' + separator
 
         if PLOT:
-            plt.savefig(title)
+            plt.savefig(title + tensor_name)
+            plt.close()
         else:
-            print(title)
-            plt.savefig(title)
-        plt.close()
+            print(title+"tensors_"+errorType+".txt")
+            file = open(title+"tensors_"+errorType+".txt", "a")
+            file.write(tensor_name+"\n")
+
+
+
