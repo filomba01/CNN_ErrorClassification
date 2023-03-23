6dd78084a7c73dddb738a9f8c85d5011ae9b9545
filippo <filo.balza2001@gmail.com>
2023-03-23 11:07:58 +0100

    improved plot tensor

--- plotTensor.py
+++ plotTensor.py
@@ -6,54 +6,65 @@ from math import sqrt
 ###
 save = False
 title = ''
+
+
 ###
 
 def split_two(num):
-	if is_square(num):
-		return int(sqrt(num)), int(sqrt(num))
+    if is_square(num):
+        return int(sqrt(num)), int(sqrt(num))
 
-	radix = int(sqrt(num))
+    radix = int(sqrt(num))
 
-	while num%radix != 0:
-		radix -= 1
+    while num % radix != 0:
+        radix -= 1
 
-	return radix, num//radix
+    return radix, num // radix
 
 
 def is_square(apositiveint):
-  x = apositiveint // 2
-  seen = set([x])
-  while x * x != apositiveint:
-    x = (x + (apositiveint // x)) // 2
-    if x in seen: return False
-    seen.add(x)
-  return True
+    x = apositiveint // 2
+    seen = set([x])
+    while x * x != apositiveint:
+        x = (x + (apositiveint // x)) // 2
+        if x in seen: return False
+        seen.add(x)
+    return True
 
 
-golden = np.load(argv[1])[0,...]
-faulty = np.load(argv[2])[0,...]
+def is_prime(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+
+golden = np.load(argv[1])[0, ...]
+faulty = np.load(argv[2])[0, ...]
+
+golden = np.reshape(golden, (13, 13, 256))
+faulty = np.reshape(faulty, (13, 13, 256))
 
 channels = golden.shape[2]
+print(golden.shape[0])
+
 x, y = split_two(channels)
 
-
 fig, axs = plt.subplots(x, y)
-
 for j in range(y):
-	for i in range(x):
+    for i in range(x):
+        diff = np.abs(golden[:, :, i + j * x] - faulty[:, :, i + j * x])
 
-		diff = np.abs(golden[:,:,i + j*x] - faulty[:,:,i + j*x])
+        diff = np.where(diff < 1e-3, 0, 1)
 
-		diff = np.where(diff < 1e-3, 0, 1)
-
-		axs[i, j].imshow(diff, cmap='hot', interpolation='nearest')
-		axs[i, j].set_yticks([])
-		axs[i, j].set_xticks([])
-		axs[i, j].set_yticklabels([])
-		axs[i, j].set_xticklabels([])
-		axs[i, j].set_title(f'Channel {i + j*x}', fontsize=8)
+        axs[i, j].imshow(diff, cmap='hot', interpolation='nearest')
+        axs[i, j].set_yticks([])
+        axs[i, j].set_xticks([])
+        axs[i, j].set_yticklabels([])
+        axs[i, j].set_xticklabels([])
+        axs[i, j].set_title(f'Channel {i + j * x}', fontsize=8)
 
 if save:
-	plt.savefig(title)
+    plt.savefig(title)
 else:
-	plt.show()
+    plt.show()
