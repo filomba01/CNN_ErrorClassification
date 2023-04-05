584e90b5d7595aaf7a982d644fb6c2e7fd758256
matteobettiati <matteo.bet.38@gmail.com>
2023-04-05 19:28:21 +0200

    automised plotTensor class

--- plotTensor.py
+++ plotTensor.py
@@ -6,8 +6,7 @@ from sys import argv, exit
 from math import sqrt
 
 ###
-save = False
-title = ''
+
 
 
 ###
@@ -40,56 +39,67 @@ def is_prime(n):
             return False
     return True
 
-
-golden = np.load(argv[1])[0, ...]
-faulty = np.load(argv[2])[0, ...]
+###
 
 if len(os.path.abspath(__file__).split('/')) > 1:
     separator = '/'
 else:
     separator = '\\'
+
+save = True
+filename = separator + os.path.abspath(__file__).split(separator)[-1]
+title = os.path.abspath(__file__).replace(filename, '') + separator + 'plotted_tensors'
+
+
+experimentPath = input("Insert the folder of the experiment: ")
 filename = separator + os.path.abspath(__file__).split(separator)[-1]
 os.path.abspath(__file__).replace(filename, '')
-experimentPath = input("Insert the name of the experiment: ")
-choosenTestFolder = input("Insert the folder: ")
-choosenTensorsF = input("Insert the subfolder: ")
-faultyTensorName = input("Insert the tensor to analyze: ")
-path = os.path.abspath(__file__).replace(filename, '') + separator + 'tensors_corrupted'+ separator + experimentPath
-path = path + separator + choosenTestFolder
-golden = np.load(path + separator + 'output_1.npy')[0,...]
-faulty = np.load(path + separator + choosenTensorsF + separator + faultyTensorName)[0,...]
+directory = os.path.abspath(__file__).replace(filename,'') + separator + 'tensors_corrupted' + separator + experimentPath
+for conv in os.listdir(directory):
+    f = directory + separator + conv
+    golden = np.load(directory + separator + conv + separator + 'output_1.npy')[0, ...]
+    for tensor in os.listdir(f):
+        if not tensor.endswith(".npy") and not tensor.endswith(".md"):
+            tensdirectory = f + separator + tensor
+            for tensor_name in os.listdir(tensdirectory):
+                if tensor_name.endswith(".npy"):
+                    faulty = np.load(tensdirectory + separator + tensor_name)[0, ...]
 
-toInvert = False
-if golden.shape[0] != golden.shape[1]:
-    toInvert = True
-    golden = np.reshape(golden, (golden.shape[2], golden.shape[1], golden.shape[0]))
-if toInvert:
-    faulty = np.reshape(faulty, (faulty.shape[2], faulty.shape[1], faulty.shape[0]))
+                    toInvert = False
+                    if golden.shape[0] != golden.shape[1]:
+                        toInvert = True
+                        golden = np.reshape(golden, (golden.shape[2], golden.shape[1], golden.shape[0]))
 
-channels = golden.shape[2]
-print(golden.shape[0])
+                    toInvertFaulty = False
+                    if faulty.shape[0] != faulty.shape[1]:
+                        toInvertFaulty = True
+                        faulty = np.reshape(faulty, (faulty.shape[2], faulty.shape[1], faulty.shape[0]))
 
-x, y = split_two(channels)
+                    channels = golden.shape[2]
 
+                    print(golden.shape[0])
 
-fig, axs = plt.subplots(x, y)
+                    if is_prime(channels):
+                        channels += 2
 
-for j in range(y):
-    for i in range(x):
-        diff = np.abs(golden[:, :, i + j * x] - faulty[:, :, i + j * x])
+                    x, y = split_two(channels)
 
-        diff = np.where(diff < 1e-3, 0, 1)
+                    fig, axs = plt.subplots(x, y)
 
-        axs[i, j].imshow(diff, cmap='hot', interpolation='nearest')
-        axs[i, j].set_yticks([])
-        axs[i, j].set_xticks([])
-        axs[i, j].set_yticklabels([])
-        axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f' {i + j * x}', fontsize=4)
+                    for j in range(y):
+                        for i in range(x):
+                            if  i + j * x < golden.shape[2]:
+                                diff = np.abs(golden[:, :, i + j * x] - faulty[:, :, i + j * x])
+                                diff = np.where(diff < 1e-3, 0, 1)
+                                axs[i, j].imshow(diff, cmap='hot', interpolation='nearest')
+                                axs[i, j].set_yticks([])
+                                axs[i, j].set_xticks([])
+                                axs[i, j].set_yticklabels([])
+                                axs[i, j].set_xticklabels([])
+                                axs[i, j].set_title(f' {i + j * x}', fontsize=4)
 
-
-
-if save:
-    plt.savefig(title)
-else:
-    plt.show()
+                    if save:
+                        plt.savefig(title+ separator + tensor_name.split('.')[0])
+                    else:
+                        plt.show()
+                    plt.close()
